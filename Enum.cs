c1a3420a42beb843999d15bbba3eca190d4a5ecd
dongminyoo <andrew@i-ve.kr>
2022-05-15 20:17:15 +0900

    study: Enum Status Code

---
+++ Enum.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+namespace Enum
+{
+    class MainApp
+    {
+        enum DialogResult { YES=200, NO, CANCLE, CONFIRM, OK }
+
+        static void Main(string[] args)
+        {
+            WriteLine((int)DialogResult.NO);
+            WriteLine((int)DialogResult.YES);
+            WriteLine((int)DialogResult.CANCLE);
+            WriteLine((int)DialogResult.CONFIRM);
+            WriteLine((int)DialogResult.OK);
+        }
+    }
+}
